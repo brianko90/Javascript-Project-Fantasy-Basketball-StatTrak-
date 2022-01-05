649751b5d705c710e3fc0cbfe088bbd738ca13cc
BrianKo <bako2466@gmail.com>
2022-01-05 10:57:33 -0800

    working on z-index to have my buttons clickable

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,9 +6,12 @@ document.addEventListener("DOMContentLoaded", async e => {
   let search = new SearchUtil();
   let fetch = new Fetch();
   let util = new Util();
+
   let teams = await fetch.fetchTeams()
   util.addTeamsToDropdown(teams)
-
+  
+  search.modalSetup();
+  
   let dropdownButton = document.querySelector('.dropContainer');
   dropdownButton.addEventListener("click", search.toggleDropdown);
 
@@ -40,23 +43,3 @@ document.addEventListener("DOMContentLoaded", async e => {
   })
   
 })
-
-var modal = document.getElementById("myModal");
-
-var btn = document.getElementById("myBtn");
-
-var span = document.getElementsByClassName("close")[0];
-
-btn.onclick = function () {
-  modal.style.display = "block";
-}
-
-span.onclick = function () {
-  modal.style.display = "none";
-}
-
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
--- src/scripts/searchUtil.js
+++ src/scripts/searchUtil.js
@@ -28,4 +28,24 @@ export class SearchUtil {
       dropdownButton.classList.add('buttonOpen');
     }
   }
+
+  modalSetup() {
+    var modal = document.getElementById("myModal");
+    var btn = document.getElementById("myBtn");
+    var span = document.getElementsByClassName("close")[0];
+
+    btn.onclick = function () {
+      modal.style.display = "block";
+    }
+
+    span.onclick = function () {
+      modal.style.display = "none";
+    }
+
+    window.onclick = function (event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+  }
 }
